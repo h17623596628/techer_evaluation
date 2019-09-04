267b11729a83246f79b5a37cc262dd93d41867ef
HYY <HYY@DESKTOP-G7V5T86>
2019-09-04 08:53:15 +0800

    asdfasdf

--- teacher_evaluation/src/com/test/domain/Teacher.java
+++ teacher_evaluation/src/com/test/domain/Teacher.java
@@ -3,10 +3,7 @@ package com.test.domain;
 public class Teacher {
 	
 	private int Id;
-<<<<<<< HEAD
-	
+
 //github.com/h17  62359662正则8/techer_evaluation.git
-=======
-    //55555555
->>>>>>> refs/remotes/origin/master
+
 }
